75ae6ad6e059ccd06a9091d820184ae3f109643f
alicja <alicjakruszewicz3@gmail.com>
2024-12-07 16:17:23 +0100

    Add Day 7

---
+++ Day7/b.hs
@@ -0,0 +1,33 @@
+import System.IO
+
+solution xs = foldl (\acc (x, y) -> acc+y) 0 filtered
+        where numbers = map words xs
+              solutions = map (toInt . init . head) numbers
+              products = map (toInts . tail)  numbers
+              combs x = getAllCombinations $ [1..x]
+              filtered = filter (\(x, y) -> any (==y) (map (getNum x) $ combs (length x - 1)) ) $ zip products solutions
+
+getNum (x:[]) [] = x
+getNum (x:y:xs) (c:cs)
+        | c == '+' = getNum (x+y:xs) cs
+        | c == '*' = getNum (x*y:xs) cs
+        | otherwise = getNum (concatNums x y:xs) cs
+
+getAllCombinations :: [x] -> [[Char]]
+getAllCombinations [] = [[]]
+getAllCombinations (x:xs) = map (\c -> c ++ ['+']) (getAllCombinations xs) ++ map (\c -> c ++ ['*']) (getAllCombinations xs) ++ map (\c -> c ++ ['|']) (getAllCombinations xs)
+
+concatNums x y = toInt (show x <> show y)
+
+
+toInts :: [[Char]] -> [Int]
+toInts xs = map toInt xs
+
+toInt :: [Char] -> Int
+toInt x = (read x) :: Int
+
+main :: IO()
+main = do 
+        handle <- openFile "input.txt" ReadMode
+        contents <- hGetContents handle
+        print $ solution $ lines contents 
